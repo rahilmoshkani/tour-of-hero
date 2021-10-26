a51c9eee853de48ea05054a48b7f05e4d863eeb6
Rahele moshkani <rahilmoshkani@gmail.com>
2021-10-26 19:38:03 +0330

    url to web api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,11 +3,18 @@ import { Injectable } from '@angular/core';
 import { HEROES } from './hero/mock-heroes';
 import { Observable,of } from 'rxjs';
 import { MessageService } from './message.service';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  constructor(private messageService:MessageService) { }
+  constructor(private messageService:MessageService,http:HttpClient) { }
+  /** log a HeroService message with the MessageService*/
+  private log(message:string){
+    this.messageService.add(`HeroService:${message}`);
+  }
+  private heroesUrl='api/heroes'; //url to web api
 
   getHeroes():Observable<HERO[]>{
   const heroes = of (HEROES);
