b2d5b21baac641b3d31ce764ec409911f2d96645
Rahele moshkani <rahilmoshkani@gmail.com>
2021-10-26 19:13:57 +0330

    getid

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -21,4 +21,12 @@ createDb(){
   return {heroes};
 
 }
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
 
+  getId(heroes:HERO[]):Number{
+    return heroes.length> 0 ? Math.max(...heroes.map(hero=>hero.id))+1:11;}
+  }
