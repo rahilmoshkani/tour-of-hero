40a26de9cf68e9689b39a340318bcafbb7c7b311
Rahele moshkani <rahilmoshkani@gmail.com>
2021-10-28 18:53:38 +0330

    add new hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -31,4 +31,13 @@ getHeroes():void{
 .subscribe(heroes =>this.heroes=heroes);
 }
 
+add(name:string):void{
+  name=name.trim();
+  if(!name){return;}
+  this.heroService.addHero({name} as HERO)
+  .subscribe(hero => {
+    this.heroes.push(hero);
+  });
+}
+
 }
