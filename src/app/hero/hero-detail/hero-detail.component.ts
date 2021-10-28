385577997c23640ffbc20994d12eab937be6e619
Rahele moshkani <rahilmoshkani@gmail.com>
2021-10-28 18:31:30 +0330

    save

--- src/app/hero/hero-detail/hero-detail.component.ts
+++ src/app/hero/hero-detail/hero-detail.component.ts
@@ -2,34 +2,36 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { HeroService } from 'src/app/hero.service';
-import {HERO}  from './hero'; 
+import { HERO } from './hero';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: ['./hero-detail.component.scss'],
 })
 export class HeroDetailComponent implements OnInit {
-   hero:HERO | undefined;
+  hero: HERO | undefined;
 
-  constructor(private route:ActivatedRoute,private heroservice:HeroService,private location:Location) { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroservice: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
-getHero():void{
-  const id=Number(this.route.snapshot.paramMap.get('id'));
-  this.heroservice.getHero(id)
-  .subscribe(hero=>this.hero=hero);
-}
-goback():void{
-  this.location.back();
-}
-save():void{
-  if (this.hero){
-    this.heroservice.updateHero(this.hero)
-    .subscribe(()=>this.goback);
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroservice.getHero(id).subscribe((hero) => (this.hero = hero));
   }
-}
+  goback(): void {
+    this.location.back();
+  }
+  save(): void {
+    if (this.hero) {
+      this.heroservice.updateHero(this.hero).subscribe(() => this.goback());
+    }
+  }
 }
